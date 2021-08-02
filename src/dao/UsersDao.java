065f0916562e2ce92e0e4290e58f44b01c6f8753
wjrHh218 <2084945328@qq.com>
2021-08-02 10:08:37 +0800

    wjr

--- src/dao/UsersDao.java
+++ src/dao/UsersDao.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface UsersDao {
     void addUser(Users user);
     void updateUser(Users user);
+
+
     void deleteUser(Users user);
     List<Users> findUsers();
     List<Users> findUsersByName(String name);
